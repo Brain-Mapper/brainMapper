662a13d0594917a902db1728b040df263a3d8f34
VZ <valentinazelayam@gmail.com>
2017-11-22 17:16:27 +0100

    adding NIfTI from an array and batch save whole collection implemented

--- ourLib/NIfTI/tester.py
+++ ourLib/NIfTI/tester.py
@@ -17,5 +17,5 @@ filenames.append('P92_SA_RMN_1.nii')
 filenames.append('P92_UL_M0_1.nii')
 
 collection= nifcoll('Dr Rech')
-collection.batchAddNIfTIs(filenames)
+collection.batchAddNIfTIsFromFiles(filenames)
 collection.printHeaders()
