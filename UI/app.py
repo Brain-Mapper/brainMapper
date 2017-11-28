e6f11f65658cccf5d1a0a9286ef0068981e36770
Graziella-Husson <graziella.husson1@gmail.com>
2017-11-28 14:33:33 +0100

    First button

--- UI/app.py
+++ UI/app.py
@@ -1,19 +1,45 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
+class UI(QtGui.QWidget):
+    
+    def __init__(self):
+        super(UI, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+        
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+        
+        self.setGeometry(300, 200, 800, 500)
+        self.setWindowTitle('BrainMapper')
+        self.setWindowIcon(QtGui.QIcon('ressources/web.png'))
 
-def initUI():
+        btnNS = QtGui.QPushButton('New Set', self)
+        btnNS.setToolTip('This is a <b>QPushButton</b> widget')
+        btnNS.setFixedWidth(150)
+        btnNS.move(1, 1)
+        btnNS.clicked.connect(self.buttonClicked)            
+
+        btnNC = QtGui.QPushButton('New Collection', self)
+        btnNC.setToolTip('This is a <b>QPushButton</b> widget')
+        btnNC.setFixedWidth(150)
+        btnNC.move(151, 1)
+        
+        self.show()
+
+    def buttonClicked(self):
+        print "ahah"
+        
+def main():
     
     app = QtGui.QApplication(sys.argv)
-
-    w = QtGui.QWidget()
-    w.resize(1000, 600)
-    w.move(200, 100)
-    w.setWindowTitle('BrainMapper')
-    w.setWindowIcon(QtGui.QIcon('ressources/web.png'))
-    w.show()
-    
+    ex = UI()
     sys.exit(app.exec_())
 
 
-initUI()
+if __name__ == '__main__':
+    main()
+
+
