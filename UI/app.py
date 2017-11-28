7041d3c3e99b1c26c06ee5bbb0ffb806cdbc8376
Graziella-Husson <graziella.husson1@gmail.com>
2017-11-28 15:21:26 +0100

    Added menu bar

--- UI/app.py
+++ UI/app.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
 
-class UI(QtGui.QWidget):
+class UI(QtGui.QMainWindow):
     
     def __init__(self):
         super(UI, self).__init__()
@@ -11,21 +11,38 @@ class UI(QtGui.QWidget):
     def initUI(self):
         
         QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
-        
+
+        exitAction = QtGui.QAction('&Exit', self)        
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(QtGui.qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
         self.setGeometry(300, 200, 800, 500)
         self.setWindowTitle('BrainMapper')
-        self.setWindowIcon(QtGui.QIcon('ressources/web.png'))
+        self.setWindowIcon(QtGui.QIcon('ressources/logo.png'))
 
-        btnNS = QtGui.QPushButton('New Set', self)
+        btnNS = QtGui.QPushButton('Create new Set', self)
         btnNS.setToolTip('This is a <b>QPushButton</b> widget')
         btnNS.setFixedWidth(150)
-        btnNS.move(1, 1)
+        btnNS.move(1, 27)
         btnNS.clicked.connect(self.buttonClicked)            
 
-        btnNC = QtGui.QPushButton('New Collection', self)
-        btnNC.setToolTip('This is a <b>QPushButton</b> widget')
-        btnNC.setFixedWidth(150)
-        btnNC.move(151, 1)
+        btnEx = QtGui.QPushButton('Import from Excel', self)
+        btnEx.setToolTip('This is a <b>QPushButton</b> widget')
+        btnEx.setFixedWidth(150)
+        btnEx.move(151, 27)
+
+
+        btnNf = QtGui.QPushButton('Import from Excel', self)
+        btnNf.setToolTip('This is a <b>QPushButton</b> widget')
+        btnNf.setFixedWidth(150)
+        btnNf.move(301, 27)
         
         self.show()
 
