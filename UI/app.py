e443c2c1765c17177957f819e8a14a93353251b5
Graziella-Husson <graziella.husson1@gmail.com>
2017-11-28 17:32:12 +0100

    Improved interface

--- UI/app.py
+++ UI/app.py
@@ -1,5 +1,27 @@
-import sys
-from PyQt4 import QtGui
+import sys, os
+from PyQt4 import QtGui, QtCore
+
+from PyQt4.Qt import *
+
+class Help(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setWindowTitle('Help')
+        self.setWindowIcon(QtGui.QIcon('ressources/help.png'))
+        self.setGeometry(QRect(100, 100, 400, 200))
+        label = QLabel(self)
+        pixmap = QPixmap(os.getcwd() + '/ressources/logo.png')
+        label.setPixmap(pixmap)
+        label.resize(pixmap.width(),pixmap.height())
+        label.move(10,10)
+        names = QLabel("Raphael Agathon & Maxime Cluchague",self)
+        names2 = QLabel("  Graziella Husson & Valentina Zelaya", self)
+        title= QLabel("BrainMapper", self)
+        names.setGeometry(QtCore.QRect(70, 110, 400, 100))
+        names2.setGeometry(QtCore.QRect(73, 130, 400, 100))
+        title.setGeometry(QtCore.QRect(210, 20, 400, 100))
+        self.show()
+
 
 class UI(QtGui.QMainWindow):
     
@@ -9,45 +31,62 @@ class UI(QtGui.QMainWindow):
         self.initUI()
         
     def initUI(self):
+        self.statusBar() # Fait apparaitre la bar en bas, ou l'on peut mettre des tips
+        menubar = self.menuBar() #Creation de la bar de menu
         
-        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
-
+        # PARAMETRES FENETRE
+        self.setGeometry(300, 200, 800, 500)
+        self.setWindowTitle('BrainMapper')
+        self.setWindowIcon(QtGui.QIcon('ressources/logo.png'))
+        
+        # DEFINITION DES ACTIONS DES MENUS
         exitAction = QtGui.QAction('&Exit', self)        
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(QtGui.qApp.quit)
 
-        self.statusBar()
+        saveAction = QtGui.QAction('&Help', self)        
+        saveAction.setShortcut('Ctrl+H')
+        saveAction.setStatusTip('Help')
+        saveAction.triggered.connect(self.showHelp)
 
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
+        setAction = QtGui.QAction('&Create new set', self)     
+        setAction.setStatusTip('Create new set')
+        setAction.triggered.connect(self.buttonClicked)
+
+        excelAction = QtGui.QAction('&Import from Excel file', self)     
+        excelAction.setStatusTip('Import from Excel file')
+        excelAction.triggered.connect(self.buttonClicked)
+
+        niftiAction = QtGui.QAction('&Import from NIfTI file', self)    
+        niftiAction.setStatusTip('Import from NIfTI file')
+        niftiAction.triggered.connect(self.buttonClicked)
+        
+        
+        # AJOUT DES ACTIONS AU MENU CORRESPONDANT
+        fileMenu = menubar.addMenu('&Program')
+        fileMenu.addAction(saveAction)
         fileMenu.addAction(exitAction)
+        SetMenu = menubar.addMenu('&New Set')
+        SetMenu.addAction(setAction)
+        SetMenu.addAction(excelAction)
+        SetMenu.addAction(niftiAction)
 
-        self.setGeometry(300, 200, 800, 500)
-        self.setWindowTitle('BrainMapper')
-        self.setWindowIcon(QtGui.QIcon('ressources/logo.png'))
-
-        btnNS = QtGui.QPushButton('Create new Set', self)
-        btnNS.setToolTip('This is a <b>QPushButton</b> widget')
+        # DEFINITION DES BOUTONS 
+        btnNS = QtGui.QPushButton('Set 1 : Speech', self)
+        btnNS.setStatusTip('Set 1 : Speech')
         btnNS.setFixedWidth(150)
         btnNS.move(1, 27)
-        btnNS.clicked.connect(self.buttonClicked)            
-
-        btnEx = QtGui.QPushButton('Import from Excel', self)
-        btnEx.setToolTip('This is a <b>QPushButton</b> widget')
-        btnEx.setFixedWidth(150)
-        btnEx.move(151, 27)
-
-
-        btnNf = QtGui.QPushButton('Import from Excel', self)
-        btnNf.setToolTip('This is a <b>QPushButton</b> widget')
-        btnNf.setFixedWidth(150)
-        btnNf.move(301, 27)
+        btnNS.clicked.connect(self.buttonClicked) # Action du bouton           
         
         self.show()
 
     def buttonClicked(self):
-        print "ahah"
+        print "Test passed. SUCCESS!"
+
+    def showHelp(self):
+        self.w = Help()
+        
         
 def main():
     
