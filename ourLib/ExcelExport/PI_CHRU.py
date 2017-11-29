985276cec1ae87fdb22a838e948d963bbaef2063
maximeCluchague <cluchagu1u@tncy-1-18-49.telecomnancy.site.univ-lorraine.fr>
2017-11-29 09:03:21 +0100

    update python script CSV

--- ourLib/ExcelExport/PI_CHRU.py
+++ ourLib/ExcelExport/PI_CHRU.py
@@ -7,6 +7,11 @@
  
 import csv
 
+#On retire les virgule des décimals par des points pour ne pas posé de problèmes avec le séparateur
+def comma_to_Point(string):
+    string2 = string.replace(',','.')
+    return string2
+
 #On créer un nouveau fichier csv avec les entêtes pour chacun des attributs
 def create_New_CSV(nomFichierCSV):
     entetes = [
@@ -22,7 +27,7 @@ def create_New_CSV(nomFichierCSV):
     ]
 
     f = open(nomFichierCSV, 'w')
-    ligneEntete = ";".join(entetes) + "\n"
+    ligneEntete = ",".join(entetes) + "\n"
     f.write(ligneEntete)
     
     print '\nfichier créer avec succès !\n'
@@ -33,7 +38,7 @@ def add_Point(nomFichierCSV,nom_Fichier_Nifti,ID_Chirurgien,ID_Patient,Localisat
     
     attributs = [nom_Fichier_Nifti,ID_Chirurgien,ID_Patient,Localisation,Type_De_Reponse,X,Y,Z,Intensite]    
     f = open(nomFichierCSV, 'a')
-    item = ";".join(attributs) + "\n"
+    item = ",".join(attributs) + "\n"
     f.write(item)
     f.close()
     #print 'ajout de l\'item [' + nom_Fichier_Nifti +','+ID_Chirurgien+','+ID_Patient+','+Type_De_Reponse+","+Localisation+','+X+','+Y+','+Z+','+Intensite + ']\n' 
@@ -52,41 +57,43 @@ def generate_CSV_point_data_file(nomFichierCSV,newCSV):
                 if ligne:
                     if ligne[0]!='':
                         ID_patient_tampon = ligne[0]
-                    add_Point(newCSV,'NULL','NULL',ID_patient_tampon,ligne[5],ligne[6],ligne[8],ligne[9],ligne[10],ligne[3])
+                    add_Point(newCSV,'NULL','NULL',ID_patient_tampon,ligne[5],ligne[6],ligne[8],ligne[9],ligne[10],comma_to_Point(ligne[3]))
             else:
                 index=index+1
                                  
     finally:
         file.close()
         
-        
-def merge_CSV(csv_1,csv_2):
+##On fait la fusion de deux csv que l'on place dans un troisieme csv résultant
+def merge_CSV(csv_1,csv_2,csvMerge):
     file1 = open(csv_1, "rb") 
     file2 = open(csv_2, "rb")   
-
+    create_New_CSV(csvMerge)
     try:
         reader1 = csv.reader(file1)
         reader2 = csv.reader(file2)
         i1=0
         i2=0
-        for ligne1 in reader1:
-            if ligne1 and i1>0:  
-                for ligne2 in reader2:
-                    if ligne2 and i2>0:
-                        if(ligne1[0]!=ligne2[0] and ligne1[1]!=ligne2[1]):
-                            
-                    else:
-                        i2=i2+1
-            else:
-                i1=i1+1                                                    
-    finally:
-        file.close()
-    
-    
-    
         
-generate_CSV_point_data_file('/home/etudiants/cluchagu1u/Téléchargements/data_CHRU.csv','/home/etudiants/cluchagu1u/Bureau/test.csv')
-
+        for ligne2 in reader2:
+            exist = False
+            print ligne2[0]
+            if ligne2 and i2>0:  
+                for ligne1 in reader1:           
+                    if ligne1 and i1>0:
+                        add_Point(csvMerge,ligne1[0],ligne1[1],ligne1[2],ligne1[3],ligne1[4],ligne1[5],ligne1[6],ligne1[7],ligne1[8])
+                        if(exist==False and ligne1[0]==ligne2[0] and ligne1[1]==ligne2[1]):
+                            exist = True
+                    i1=i1+1
+                if exist!=True:
+                    add_Point(csvMerge,ligne2[0],ligne2[1],ligne2[2],ligne2[3],ligne2[4],ligne2[5],ligne2[6],ligne2[7],ligne2[8])
+            i2=i2+1                                                       
+    finally:
+        file1.close()
+        file2.close()  
+    
+merge_CSV('/home/etudiants/cluchagu1u/Bureau/testMerge1.csv','/home/etudiants/cluchagu1u/Bureau/testMerge2.csv','/home/etudiants/cluchagu1u/Bureau/testMerge3.csv')
+#generate_CSV_point_data_file('/home/etudiants/cluchagu1u/Téléchargements/data_CHRU.csv','/home/etudiants/cluchagu1u/Bureau/testMerge2.csv')
 #create_New_CSV('/home/etudiants/cluchagu1u/Bureau/test.csv')   
 #add_Point('/home/etudiants/cluchagu1u/Bureau/test.csv','test1','test1','test1','test1','test1','test1','test1','test1','test1')
 #add_Point('/home/etudiants/cluchagu1u/Bureau/test.csv','test2','test2','test2','test2','test2','test2','test2','test2','test2')
