deecc3f0d1edd8b62111517ac34ade19402cfa4a
VZ <valentinazelayam@gmail.com>
2017-12-18 01:48:37 +0100

    adding a function to test whether a NIfTi image contains only binary values (0 and 1)

--- ourLib/test/nifimage_test.py
+++ ourLib/test/nifimage_test.py
@@ -32,6 +32,12 @@ print(ni_patient.get_img_class())
 #print data
 print(ni_patient.get_copy_img_data())
 print(ni3.get_copy_img_data(True)>0)
+
+## Testing if given image contains only 0 and 1
+print('patient binary ? '+ str(ni_patient.is_binary_image()))  #expected :false
+
+print('P92_UL_RMI1 binary ? '+ str(ni3.is_binary_image()))     #expected : true
+
 us_data=ni3.get_copy_img_data(True)[ni3.get_copy_img_data(True)>0].T
 print(us_data)
 print(us_data[122])
