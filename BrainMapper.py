36088b970ca3a1487048a019658fe27f6aefdf97
Graziella-Husson <graziella.husson1@gmail.com>
2018-01-03 19:01:18 +0100

    vizu of collection + selection (not working yet)

--- BrainMapper.py
+++ BrainMapper.py
@@ -5,6 +5,9 @@ from ourLib.dataExtraction import extractor as xt
 
 import threading as th
 
+global selected
+selected = []
+
 def open_nifti(path):
     image = ni.from_file(path)
     print(image.get_affine_matrix())
@@ -15,17 +18,23 @@ def open_nifti(path):
 
     #draw_glass_thread = th.Thread(target=image.plot_glass())
     #draw_glass_thread.start()
+    return image
 
 def do_image_collection(files):
     coll = ImageCollection()
     for file in files:
-        image = ni.from_file(str(file))
+        image = open_nifti(str(file))
         coll.add(image)
-    list = coll.get_img_list()
-    for item in list :
-        print item.get_affine_matrix()
     #extracted_data = xt.extract_from_collection(coll)
     #print(extracted_data)
     return coll
     
+def add_coll(coll):
+    selected.append(coll)
     
+def rm_coll(coll):
+    selected.remove(coll)
+    
+def get_selected():
+    return selected
+
