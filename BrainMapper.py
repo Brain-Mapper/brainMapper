b0ff2b9e7941b660d291a0e63fc5adb867b863a3
VZ <valentinazelayam@gmail.com>
2018-02-12 19:43:09 +0100

    [doc] Added documentation to BrainMapper.py

--- BrainMapper.py
+++ BrainMapper.py
@@ -1,3 +1,17 @@
+# NAME
+#        BrainMapper
+#
+# DESCRIPTION
+#
+#       The module 'BrainMapper' is the controller of our application : it links the user interface and the library
+#       that handles NIfTIs
+#
+# HISTORY
+#
+# 28 november 2017 - Initial design and coding. (@Graziella-Husson, Graziella Husson)
+# 12 february 2018 - Added documentation (@vz-chameleon, Valentina Zelaya)
+
+
 from ourLib.niftiHandlers.nifimage import NifImage
 from ourLib.niftiHandlers.imagecollection import ImageCollection
 from ourLib.niftiHandlers.set import Set
@@ -33,22 +47,26 @@ with open('ressources/clustering_data/clustering_algorithms_available.json', 'r'
 # Global variable for currently selected clustering method
 currentClusteringMethod = None
 
-
 # Global variables for calculation results
 # currentCalculationResult = None
 
 
 def open_nifti(path):
-    # --- Opens a nifti file from path
-
+    """
+    Opens a NIfTI file from path
+    :param path: A string of the file path
+    :return: NifImage instance
+    """
     image = NifImage.from_file(path)
-    # print(image.get_affine_matrix())
     return image
 
 
 def do_image_collection(files):
-    # --- Create an image collection from a lits of file path
-
+    """
+    Create an image collection from a lits of file path
+    :param files: list of strings (file paths)
+    :return: ImageCollection instance
+    """
     coll = ImageCollection("default", currentSet)
     # We want an unique name for each collection
     # To do so we use the object ID
@@ -58,26 +76,35 @@ def do_image_collection(files):
     for file in files:
         image = open_nifti(str(file))
         coll.add(image)
+
     add_coll(coll)  # We add the collection create to selected by default
     currentSet.add_collection(coll)  # We add the collection created in the current set
     return coll
 
 
 def add_coll(coll):
-    # --- Add a collection to the selected ones
-
+    """
+    Add a collection to the selected collection list [global variable 'selected']
+    :param coll: ImageCollection instance
+    :return: Nothing
+    """
     selected.append(coll)
 
 
 def rm_coll(coll):
-    # --- Remove a collection to the selected ones
-
+    """
+    Remove a collection from the selected collection list [global variable 'selected']
+    :param coll: ImageCollection instance
+    :return: Nothing
+    """
     selected.remove(coll)
 
 
 def get_selected():
-    # --- Return the selected collections (usefull for all views that use data)
-
+    """
+    Return the selected collections (useful for all views that use data)
+    :return: global variable 'selected'
+    """
     return selected
 
 
@@ -92,9 +119,11 @@ def export_excel():
 
 
 def extract_data_from_selected():
-    # --- Extract the interesting data from the selected image collections using extractor's module functions
-    # --- :return: A UsableDataSet instance (see dataExtraction.usable_data)
-
+    """
+    Extract the interesting data from the selected image collections using extractor's module functions.
+    Put this data in the global variable 'currentUsableDataSet'
+    :return: Nothing. Global var 'currentUsableDataset' is modified
+    """
     for x in selected:
         print(type(x))
     global currentUsableDataset
@@ -102,9 +131,12 @@ def extract_data_from_selected():
 
 
 def extract_data_as_centroids_from_selected():
-    # --- Extract the interesting data from the selected image collections using extractor's module functions
-    # --- :return: A UsableDataSet instance (see dataExtraction.usable_data)
-
+    """
+      Extract the interesting data from the selected image collections using extractor's module functions
+      and only centroids as points from each file. (one centroid per file)
+      Put this data in the global variable 'currentUsableDataSet'
+      :return: Nothing. Global var 'currentUsableDataset' is modified
+    """
     global currentUsableDataset
     currentUsableDataset = xt.extract_from_collection_list_using_centroids(selected)
 
@@ -155,61 +187,84 @@ def run_calculation(selectedAlgorithm, nifti_collection, arguments):
 
 
 def get_selected_from_name(name):
-    # --- Return the selected collection named "name"
-
+    """
+    Returns the selected collection named "name" in the selected image collections list
+    :param name: The collection that we look for (unique ID)
+    :return: ImageCollection
+    """
     for x in selected:
         if (name == x.name):
             return x
 
 
 def get_toRM():
-    # --- Return list of images to remove (usefull for edit view -> save changes"
-
+    """
+    :return: Returns list of images to remove (useful for edit view -> save changes)
+    """
     return toRM
 
 
 def add_toRM(im):
-    # --- Add an image to remove in the list toRM (usefull for all views that use data)
-
+    """
+    Add an image to remove in the list toRM (useful for all views that use data)
+    :param im: NifImage instance
+    :return: Nothing
+    """
     toRM.append(im)
 
 
 def rm_toRM(im):
-    # --- Remove an image to remove from the list toRM (usefull for all views that use data)
-
+    """
+    Remove an image to remove from the list toRM (useful for all views that use data)
+    :param im: NifImage instance
+    :return: Nothing
+    """
     toRM.remove(im)
 
 
 def reset_toRM():
-    # --- Reset the list toRM (usefull for all views that use data and allow the list to be used somewhere else)
-
+    """
+    Reset the list toRM (usefull for all views that use data and allow the list to be used somewhere else)
+    :return: Nothing
+    """
     del toRM[:]
 
 
 def set_current_coll(coll):
-    # --- Set the current collection (usefull to show the collection selected in edit view)
-
+    """
+    Set the current collection [global variable] (usefull to show the collection selected in edit view)
+    :param coll: ImageCollection instance
+    :return: nothing
+    """
     global current_collec
     current_collec = coll
 
 
 def get_current_coll():
-    # --- Get te current collection
-
+    """
+    Get the current collection [global variable]
+    :return: Global variable current_collec
+    """
     global current_collec
     return current_collec
 
 
 def set_current_coll_name(name):
-    # --- Set the current collection's name (usefull to rename the collection selected in edit view)
-
+    """
+    Set the current collection's name (useful to rename the collection selected in edit view)
+    :param name: A string that will be the new name of the collection
+    :return: Nothing
+    """
     cur = get_current_coll()
     cur.set_name(name)
 
 
 def exists_selected(name):
-    # --- Returns True if the collection's name "name" is already used by an other one in selected
-
+    """
+    Returns True if the collection's name "name" is already used by an other one in selected collections list (global var 'selected')
+    :param name: The collections' name to be tested
+    :return: Boolean
+    """
     for i in selected:
         if (i.name == name):
             return True
@@ -217,8 +272,11 @@ def exists_selected(name):
 
 
 def exists_coll_in_sets(name):
-    # --- Returns True if the collection's name "name" is already used in one of the sets we have
-
+    """
+    Returns True if the collection's name "name" is already used in one of the sets we have
+    :param name: The collections' name to be tested
+    :return: Boolean
+    """
     sets = get_all_sets()
     for s in sets:
         collecs = s.get_coll()
@@ -229,15 +287,21 @@ def exists_coll_in_sets(name):
 
 
 def add_image_coll(coll, files):
-    # --- Add an image from a file path in a given collection
-
+    """
+    Add all images from a file paths list in a given collection
+    :param coll: ImageCollection instance
+    :param files: A list of file path's = Images to add
+    :return: Nothing (modifies given collection)
+    """
     for file in files:
         coll.add_from_file(str(file))
 
 
 def delete_current_coll():
-    # --- Delete the current collection from its set and from the app
-
+    """
+    Delete the current collection from its set and from the app
+    :return: Nothing
+    """
     coll = get_current_coll()
     this_set = coll.getSetName()
     rm_coll(coll)
@@ -249,8 +313,10 @@ def delete_current_coll():
 
 
 def save_modifs():
-    # --- Apply the changes the user made in the edit view (use toRM to know the images to remove from the current collection)
-
+    """
+    Apply the changes the user made in the edit view (use toRM to know the images to remove from the current collection)
+    :return: Nothing
+    """
     global current_collec
     for i in toRM:
         current_collec.remove(i.filename)
@@ -258,8 +324,11 @@ def save_modifs():
 
 
 def exists_set(name):
-    # --- Return True if the set's name "name" is already used by another set
-
+    """
+    Return True if the set's name "name" is already used by another set
+    :param name: The tested set's name
+    :return: Boolean
+    """
     for i in sets:
         if (i.name == name):
             return True
@@ -267,8 +336,11 @@ def exists_set(name):
 
 
 def newSet(name):
-    # --- Creates a new set a the name "name" and add it into the set list. Also change the current set with the new one
-
+    """
+    Creates a new set a the name "name" and add it into the set list. Also change the current set with the new one
+    :param name: The new set's name
+    :return: Set instance
+    """
     global currentSet
     new_set = Set(name)
     sets.append(new_set)
@@ -277,14 +349,21 @@ def newSet(name):
 
 
 def set_current_set(new_set):
-    # --- Set the current set with new_set
-
+    """
+    Set the current set with new_set
+    :param new_set: The set to which we have to set the current set
+    :return: Nothing (changes global 'currentSet')
+    """
     global currentSet
     currentSet = new_set
 
 
 def creation_date(path_to_file):
-    # --- Return the creation date for the file located at path_to_file
+    """
+    Return the creation date for the file located at path_to_file
+    :param path_to_file: string of file path
+    :return: Date
+    """
     filename, file_extension = os.path.splitext(path_to_file)
     if file_extension == ".csv":
         print(time.time())
@@ -302,45 +381,64 @@ def creation_date(path_to_file):
 
 
 def add_set(my_set):
-    # --- Add my_set to the sets
-
+    """
+    Add my_set to the sets list
+    :param my_set: Set Instance to add
+    :return: Nothing
+    """
     sets.append(my_set)
 
+
 def rm_set(my_set):
-    # --- Remove my_set from the sets
+    """
+    Remove my_set from the sets list
+    :param my_set: Set instance to remove
+    :return: Nothing
+    """
     sets.remove(my_set)
 
 
 def get_current_vizu():
-    # --- Return the currentVizu
-
+    """
+    Return the currentVisualisation (variable currentVizu)
+    :return:
+    """
     global currentVizu
     return currentVizu
 
 
 def set_current_vizu(collView):
-    # --- Set the current vizu with the vizu collView
-
+    """
+    Set the current vizu with the vizu collView
+    :param collView:
+    :return:
+    """
     global currentVizu
     currentVizu = collView
 
 
 def get_current_set():
-    # --- Return the current set
-
+    """
+    Return the current set [global var 'currentSet']
+    :return:
+    """
     global currentSet
     return currentSet
 
 
 def get_all_sets():
-    # --- Return all the sets that exists in the app
-
+    """
+    :return: Return all the sets that exists in the app [global var 'sets']
+    """
     return sets
 
 
 def setColNameInSet(name):
-    # --- Rename the current collection with the name "name". Even in its set and in selected
-
+    """
+    Rename the current collection with the name "name". Even in its set and in selected
+    :param name: new name for collection
+    :return:
+    """
     old = get_current_coll()
     rm_coll(old)
     this_set = old.getSetName()
@@ -350,29 +448,56 @@ def setColNameInSet(name):
     add_coll(cur_col)
 
 
-# --- currently selected clustering method ---
 def set_selected_clustering_method(method_name):
+    """
+    Set the currently selected clustering method
+    :param method_name: A string (the clustering method name)
+    :return: Nothing (sets global current clustering method)
+    """
     global currentClusteringMethod
     currentClusteringMethod = method_name
 
 
 def get_selected_clustering_info():
+    """
+    Get the selected clustering method information from method dictionnary (loaded from json file)
+    :return:
+    """
     if currentClusteringMethod is not None:
         return app_clustering_available[currentClusteringMethod]
     else:
         return None
 
+
 def makeClusterResultSet(a_usable_dataset, label):
+    """
+    Make results from clustering as sets and image collections
+    :param a_usable_dataset: The data of the set, a UsableDataSet instance
+    :param label: cluster label
+    :return:
+    """
     new_set = uds.extract_set_images_by_cluster(a_usable_dataset, label, 'ressources/template_mni/mni_icbm152_t1_tal_nlin_asym_09a.nii')
     add_set(new_set)
     clusteringsets.append(new_set)
 
+
 def getClusterResultSets():
+    """
+    :return: Global variable clustering sets, containing the clustering results as a set
+    """
     return clusteringsets
 
+
 def rmClusterResultSets(s):
+    """
+    Remove a set from clustering results set
+    :param s: The set to remove
+    :return: Nothing
+    """
     clusteringsets.remove(s)
 
+
+# ---- IMPORT ----
 def simple_import(csv_file_path, template_mni_path):
     coll = imp.simple_import(csv_file_path, template_mni_path, currentSet)
     add_coll(coll)
